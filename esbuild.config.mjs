d3e164ed47a7983cd955eaa13e525d8bb33a0229
gabmart1995 <gabmart1995@gmail.com>
2025-06-24 18:12:05 -0400

    wip: preparacion para el entorno web PWA

---
+++ esbuild.config.mjs
@@ -0,0 +1,28 @@
+import * as esbuild from 'esbuild';
+
+esbuild.context({
+    entryPoints: ['./src/index.ts'],
+    outdir: './www',
+    bundle: true,
+    write: true,
+    minifyWhitespace: true,
+    minifySyntax: true,
+    minifyIdentifiers: true,
+    sourcemap: 'linked',
+    tsconfig: './tsconfig.json',
+    target: ['chrome58', 'firefox57']
+})
+    .then(ctx => {
+        // inicia el revision de los archivos
+        ctx.watch()
+            .catch(console.error);
+        
+        // inicia el servidor de desarrollo
+        ctx.serve({
+            port: 3000,
+            servedir: 'www',
+            fallback: './www/index.html'
+        })
+            .then(server => console.log('Servidor corriendo en el ' + server.port))
+            .catch(console.error);
+    });
